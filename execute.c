41bac4746bec951205c99d768b29354ec136ff0a
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-04-21 10:41:58 +0200

    fix: correct command execution path handling in execute function

--- execute.c
+++ execute.c
@@ -8,23 +8,38 @@
  */
 void execute(char **args)
 {
-	pid_t	pid;
+	pid_t pid;
 	int	status;
+	char *cmd_path;
 
+	if (strchr(args[0], '/') != NULL)
+		cmd_path = strdup(args[0]);
+	else
+		cmd_path = get_path(args[0]);
+	if (cmd_path == NULL)
+	{
+		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+		return;
+	}
 	pid = fork();
 	if (pid == -1)
 	{
 		perror("fork");
+		free(cmd_path);
 		return;
 	}
 	if (pid == 0)
 	{
-		if (execve(args[0], args, environ) == -1)
+		if (execve(cmd_path, args, environ) == -1)
 		{
 			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+			free(cmd_path);
 			_exit(127);
 		}
 	}
 	else
+	{
 		waitpid(pid, &status, 0);
+		free(cmd_path);
+	}
 }
