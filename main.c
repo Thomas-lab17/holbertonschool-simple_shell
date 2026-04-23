de8ecd768c4314bbde43d57bdfb64539e5f074bd
artemis-bunbun <starfruitworld@gmail.com>
2026-04-23 15:02:27 +0000

    fix: resolve main.c merge markers

--- main.c
+++ main.c
@@ -1,23 +1,5 @@
 #include "main.h"
 
-<<<<<<< HEAD
-char *program_name = NULL;
-
-/**
- * main - Entry point of the shell program.
- * @argc: argument count (unused)
- * @argv: argument vector; argv[0] is used as the program name in errors
- *
- * Return: The exit status of the last command executed.
- */
-int main(int argc, char **argv)
-{
-	char	*input;
-	char	**args;
-	int	status = 0;
-=======
-int main(void)
-{
 char *program_name = NULL;
 
 /**
